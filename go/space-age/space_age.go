afcf9244087c56f9d3aaee57062466a9a1bd1cab
kumadee <deepak.kumar.ext@gmail.com>
2021-02-11 13:03:26 +0000

    DRY changes

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -8,22 +8,15 @@ const EarthYearInSeconds float64 = 31557600.0
 
 //Age converts the age on a planet given in seconds to earth-years
 func Age(seconds float64, planet Planet) float64 {
-	switch planet {
-	case "Mercury":
-		return seconds / (0.2408467 * EarthYearInSeconds)
-	case "Venus":
-		return seconds / (0.61519726 * EarthYearInSeconds)
-	default:
-		return seconds / EarthYearInSeconds
-	case "Mars":
-		return seconds / (1.8808158 * EarthYearInSeconds)
-	case "Jupiter":
-		return seconds / (11.862615 * EarthYearInSeconds)
-	case "Saturn":
-		return seconds / (29.447498 * EarthYearInSeconds)
-	case "Uranus":
-		return seconds / (84.016846 * EarthYearInSeconds)
-	case "Neptune":
-		return seconds / (164.79132 * EarthYearInSeconds)
+	PlanetEarthYears := map[Planet]float64{
+		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Earth":   1,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
 	}
+	return seconds / (PlanetEarthYears[planet] * EarthYearInSeconds)
 }
